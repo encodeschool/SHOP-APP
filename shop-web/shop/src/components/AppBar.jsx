70f3c57c3359b72f58c48b5d87abcfa39abf6844
Ergashev Nurmukhammad From WORK PC <nergashev.evolution@gmail.com>
2025-08-18 16:46:12 +0300

    Updating yml for web

--- shop-web/shop/src/components/AppBar.jsx
+++ shop-web/shop/src/components/AppBar.jsx
@@ -102,7 +102,7 @@ export default function AppBar() {
                     <button onClick={handleLogout} className="block px-4 py-2 hover:bg-gray-100">Logout</button>
                   </>
                 ) : (
-                  <Link to="/login" className="block px-4 py-2 hover:bg-gray-100">Login</Link>
+                  <Link to="/login" className="block px-4 py-2 hover:bg-gray-100">{t('login')}</Link>
                 )}
               </div>
             )}
