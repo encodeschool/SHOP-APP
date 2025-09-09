1d0383f0ef42322b8c7a50baa5399294212c203f
Nurmukhammad Ergashev <nergashev@evolution.com>
2025-09-09 13:46:20 +0100

    Some changes in category page

--- shop-web/shop/src/components/CategoryCard.jsx
+++ shop-web/shop/src/components/CategoryCard.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CategoryCard = ({ category }) => {
+  const BASE_URL = process.env.REACT_APP_BASE_URL || '';
   return (
     <Link to={`/category/${category.id}`}>
       <div className="border rounded-lg shadow-lg overflow-hidden bg-white hover:shadow-xl transition-shadow duration-300">
         <div className="flex flex-col items-center p-6 text-center">
           <div className="w-16 h-16 mb-4 rounded-full bg-gray-200 flex items-center justify-center text-3xl text-gray-700">
             {/* Displaying an icon if available, or a fallback */}
-            {category.icon ? (
-              <img src={category.icon} alt={category.name} className="w-full h-full object-contain" />
+            {category.icon? (
+              <img
+                    src={
+                    category.icon
+                        ? `${BASE_URL}${category.icon}`
+                        : '/placeholder.jpg'
+                    }
+                    alt={category.name}
+                    className="w-[35px] object-contain"
+              />
             ) : (
               <span></span> // Placeholder emoji
             )}
