6ad7ce30e59e73fa07fc030a9a619ecabdf1890b
Nurmukhammad Ergashev <nergashev@evolution.com>
2025-09-09 09:08:44 +0100

    Mega Menu updates

--- shop-web/shop/src/components/MegaMenu.jsx
+++ shop-web/shop/src/components/MegaMenu.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const MegaMenu = ({ categories, getLocalizedName, BASE_URL }) => {
   return (
-    <div className="absolute left-0 right-0 bg-white text-black shadow-lg z-[100] flex p-6 rounded-b-xl border-t border-gray-200">
-      <div className="flex-1 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
+    <div className="absolute left-0 right-0 bg-white text-black shadow-lg z-[100] flex flex-wrap p-6 rounded-b-xl border-t border-gray-200">
+      <div className="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {categories.map((category) => (
           <div key={category.id} className="flex flex-col">
             <h3 className="text-lg font-bold text-indigo-500 mb-2">
@@ -38,6 +38,7 @@ const MegaMenu = ({ categories, getLocalizedName, BASE_URL }) => {
           </div>
         ))}
       </div>
+      <img src="https://png.pngtree.com/png-clipart/20230418/original/pngtree-you-are-here-with-map-pointer-png-image_9065739.png" className='object-cover md:w-[25%]' alt="" />
     </div>
   );
 };
