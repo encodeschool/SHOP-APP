c8e211444394432aa21974c287f4abcd717462fd
Nurmukhammad Ergashev <nergashev@evolution.com>
2025-09-11 09:05:29 +0100

    Scrolling and fixing megamenu

--- projects/goshtgo/shop/src/components/CategoryBar.jsx
+++ projects/goshtgo/shop/src/components/CategoryBar.jsx
@@ -305,7 +305,7 @@ const CategoryBar = () => {
       <div
         className={`fixed inset-0 bg-white z-50 transform transition-transform duration-300 ease-in-out ${
           mobileMenuOpen ? 'translate-x-0' : 'translate-x-full'
-        } md:hidden`}
+        }  md:hidden overflow-y-auto max-h-screen`}
       >
         <div className="flex items-center justify-between p-4 border-b">
           <Link to="/" className="text-3xl font-bold flex items-center justify-center">
